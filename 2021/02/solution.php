c4fdb4e945bc4c4d489921d1a83a96ae4bb6419a
ಠ_ಠ <easteregg@verfriemelt.org>
2022-01-17 20:39:55 +0100

    day 2

--- 2021/02/solution.php
+++ 2021/02/solution.php
@@ -1,5 +1,30 @@
 <?php
 
-    declare( strict_types = 1 );
+    $input = explode( "\n", file_get_contents( 'input' ) ?: '' );
+//    $input = explode( "\n", file_get_contents( 'input.simple' ) ?: '' );
 
-    
+    $position = 0;
+    $depth    = 0;
+
+    foreach ( $input as $command ) {
+        [$command, $amount] = explode( ' ', $command );
+
+        switch ( $command ) {
+            case 'forward':
+                $position += (int) $amount;
+                break;
+            case 'down':
+                $depth    += (int) $amount;
+                break;
+            case 'up':
+                $depth    -= (int) $amount;
+                break;
+        }
+    }
+
+    print_r( [
+        'horizontal' => $position,
+        'depth'      => $depth,
+        'product'    => $position * $depth,
+    ] );
+
