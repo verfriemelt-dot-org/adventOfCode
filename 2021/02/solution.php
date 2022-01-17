495c2ee5ffceb5c7bf60421f760fc31c3e6cc051
ಠ_ಠ <easteregg@verfriemelt.org>
2022-01-17 20:45:55 +0100

    day 2 part 2

--- 2021/02/solution.php
+++ 2021/02/solution.php
@@ -3,28 +3,47 @@
     $input = explode( "\n", file_get_contents( 'input' ) ?: '' );
 //    $input = explode( "\n", file_get_contents( 'input.simple' ) ?: '' );
 
-    $position = 0;
-    $depth    = 0;
+    $p1 = [
+        'horizontalPosition' => 0,
+        'depth'              => 0,
+    ];
+
+    $p2 = [
+        'aim'                => 0,
+        'horizontalPosition' => 0,
+        'depth'              => 0,
+    ];
 
     foreach ( $input as $command ) {
         [$command, $amount] = explode( ' ', $command );
 
         switch ( $command ) {
             case 'forward':
-                $position += (int) $amount;
+                $p1['horizontalPosition'] += (int) $amount;
+                $p2['horizontalPosition'] += (int) $amount;
+                $p2['depth']              += $p2['aim'] * (int) $amount;
                 break;
             case 'down':
-                $depth    += (int) $amount;
+                $p1['depth']              += (int) $amount;
+                $p2['aim']                += (int) $amount;
                 break;
             case 'up':
-                $depth    -= (int) $amount;
+                $p1['depth']              -= (int) $amount;
+                $p2['aim']                -= (int) $amount;
                 break;
         }
     }
 
     print_r( [
-        'horizontal' => $position,
-        'depth'      => $depth,
-        'product'    => $position * $depth,
+        'horizontal' => $p1['horizontalPosition'],
+        'depth'      => $p1['depth'],
+        'product'    => $p1['horizontalPosition'] * $p1['depth'],
+    ] );
+
+    print_r( [
+        'horizontal' => $p2['horizontalPosition'],
+        'depth'      => $p2['depth'],
+        'aim'        => $p2['aim'],
+        'product'    => $p2['horizontalPosition'] * $p2['depth'],
     ] );
 
