d39decd69d03c0e370a68bdec5c0146f4b9fba41
ಠ_ಠ <easteregg@verfriemelt.org>
2022-01-23 14:45:56 +0100

    day 8 part 1

---
+++ 2021/08/solution.php
@@ -0,0 +1,32 @@
+<?php
+
+    declare( strict_types = 1 );
+
+    $input = explode( "\n", file_get_contents( __DIR__ . '/input' ) ?: '' );
+
+//    $input = explode( "\n", file_get_contents( __DIR__ . '/input.simple' ) ?: '' );
+
+    function sortString( string $in ): string {
+        $data = str_split( $in );
+        sort( $data );
+        return implode( '', $data );
+    }
+
+    $simpleDigitsCount = 0;
+    foreach ( $input as $line ) {
+
+        [$in, $out] = explode( "|", $line, 2 );
+        $in  = explode( " ", trim( $in ) );
+        $out = explode( " ", trim( $out ) );
+
+        foreach ( $out as $digit ) {
+            if ( in_array( strlen( $digit ), [ 2, 3, 4, 7 ], true ) ) {
+                $simpleDigitsCount++;
+            }
+        }
+    }
+
+    print_r( [
+        '1,4,7,8 count: ' => $simpleDigitsCount
+    ] );
+
