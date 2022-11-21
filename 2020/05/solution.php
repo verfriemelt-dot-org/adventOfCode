49c6e63a99b4cd827ea4aae71f95b298f64af0f7
ಠ_ಠ <easteregg@verfriemelt.org>
2022-11-21 07:33:02 +0100

    2020.12.05

---
+++ 2020/05/solution.php
@@ -0,0 +1,51 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace year2020\day4;
+
+use function array_fill;
+use function array_keys;
+use function str_split;
+
+return static function ( string $input ): array {
+
+    $input = explode( "\n", $input );
+
+    [ $part1, $part2 ] = [ null, null ];
+
+    $maxSeatId = 0;
+
+    $seats = array_fill(8*10 + 7, 883 - (8*10 + 7) , true);
+
+    foreach ( $input as $line ) {
+        $row = bindec(
+            implode(
+                '',
+                array_map( fn ( string $c ): string => $c === 'F' ? "0" : "1", array_slice( str_split( $line ), 0,7 ) )
+            )
+        );
+        $column = bindec(
+            implode(
+                '',
+                array_map( fn ( string $c ): string => $c === 'L' ? "0" : "1", array_slice( str_split( $line ), 7,9 ) )
+            )
+        );
+
+        // remove seat from list
+        foreach($seats as $idx => $id) {
+            if ($idx === $row*8+$column) {
+                unset($seats[$idx]);
+                break;
+            }
+        }
+
+        $maxSeatId = max($row*8+$column,$maxSeatId);
+    }
+
+    return [
+        'part1' => $maxSeatId,
+        'part2' => count($input) === 1 ? null : current(array_keys($seats)),
+    ];
+};
+
